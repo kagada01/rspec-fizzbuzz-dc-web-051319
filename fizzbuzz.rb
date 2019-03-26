b56e84477b5d0516f8d8c8d67ee137d095124bc4
Dan Kagan <dankagan7@gmail.com>
2019-03-26 20:29:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(number)
   if (((number % 3) == 0) && ((number % 5) == 0))
     fb = "FizzBuzz"
-    fb
+      fb
     elsif ((number % 3) == 0)
       fizz = "Fizz"
-      fizz
+        fizz
       elsif ((number % 5) == 0)
         buzz = "Buzz"
-        buzz
+          buzz
         else ( (( number % 3 ) !==0 ) || (( number % 5 ) !== 0 ))
           
   end
