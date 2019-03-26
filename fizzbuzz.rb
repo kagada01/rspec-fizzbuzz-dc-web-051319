a8f0905c6202bae46ebf293367e64e8b8db0234e
Dan Kagan <dankagan7@gmail.com>
2019-03-26 20:25:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,8 @@ def fizzbuzz(number)
   if ((number % 3 == 0) && (number % 5 == 0))
     fb = "FizzBuzz"
     fb
-  elsif (number % 3 == 0)
+    elsif (number % 3 == 0)
+    fizz = "Fizz"
+    fizz
   end
 end
