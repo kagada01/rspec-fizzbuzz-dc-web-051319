e5c5669b708bc1d7086d3b9bf6ccff960ed1d415
Dan Kagan <dankagan7@gmail.com>
2019-03-26 20:30:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,2 @@
 def fizzbuzz(number)
-  if (((number % 3) == 0) && ((number % 5) == 0))
-    fb = "FizzBuzz"
-      fb
-    elsif ((number % 3) == 0)
-      fizz = "Fizz"
-        fizz
-      elsif ((number % 5) == 0)
-        buzz = "Buzz"
-          buzz
-        else ( (( number % 3 ) !==0 ) || (( number % 5 ) !== 0 ))
-          nil
-  end
 end
